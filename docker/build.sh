22b74f845453dba2b9bcf8ee781086fd1c754453
JiangRensheng <13841495@qq.com>
2019-03-20 13:56:18 +0800

    add docker build support

---
+++ docker/build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+if [ ! -d "/opt/mptcp" ]; then
+    mkdir -p /opt 
+    git clone https://github.com/golden-finger/mptcp.git /opt/mptcp
+fi
+
+pushd /opt/mptcp
+make defconfig 
+make localmodconfig 
+patch -p0 .config .config.mptcp 
+make-kpkg  --initrd --revision 18.04 --append-to-version -20190320 kernel_image
+
