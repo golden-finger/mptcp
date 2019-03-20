6ed009cf8f73e0d07b45d250c6ce0fdae1d5c91b
JiangRensheng <13841495@qq.com>
2019-03-20 14:11:45 +0800

    make-kpkg 环境准备好

--- docker/build.sh
+++ docker/build.sh
@@ -8,6 +8,6 @@ fi
 pushd /opt/mptcp
 make defconfig 
 make localmodconfig 
-patch -p0 .config .config.mptcp 
+patch -p0 .config docker/.config.patch 
 make-kpkg  --initrd --revision 18.04 --append-to-version -20190320 kernel_image
 
